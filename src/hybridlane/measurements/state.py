47ea3e9346886f4ddd7a97dfe86eaf97f5443e22
b-burgstahler <50333834+b-burgstahler@users.noreply.github.com>
2026-02-19 16:10:44 -0500

    expand state docstring

--- src/hybridlane/measurements/state.py
+++ src/hybridlane/measurements/state.py
@@ -9,7 +9,31 @@ from .base import Truncation
 
 
 def state(wires: Wires | None = None) -> "StateMP":
-    """State measurement process."""
+    """State measurement process.
+    Analogous to Pennylane's state measurement (qml.state()).
+    Two major notes:
+    1) The logic of this measurement process should be handled by the device since the state is entirely dependent on the fock cutoffs set by the device.
+    2) The device level implementation should preserve Pennylane's lexicographical ordering -- ie the 'top' wire is the most significant bit in the statevector.
+
+
+    Example (assume fock cutoffs of 4 for each mode):
+    ```python
+        def circuit():
+            hqml.FockState(  # set mode 1 to fock state 1 using qubit 0 as ancilla
+                1, [0, 1]
+            )
+            hqml.FockState(  # set mode 2 to fock state 2 using qubit 0 as ancilla
+                2, [0, 2]
+            )
+            return hqml.state(),
+    ```
+
+    The returned statevector should correspond to the state |0,1,2> (with cutoffs of 2,4,4)
+    so the state |0,1,2> corresponds to the index 6 in the statevector (since (0)*4*4 + (1)*4 + (2)*1 = 6).
+
+    >>>
+
+    """
 
     return StateMP(wires=wires)
 
