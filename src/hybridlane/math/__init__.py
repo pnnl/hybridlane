f6fefa5b2a2a9adb4b99ce8202f14a781e046830
Jim Furches <38408451+jfurches@users.noreply.github.com>
2026-05-05 17:39:56 -0400

    feat: `math.dag`

--- src/hybridlane/math/__init__.py
+++ src/hybridlane/math/__init__.py
@@ -2,9 +2,12 @@
 # SPDX-License-Identifier: BSD-2-Clause
 r"""A wrapper around :mod:`pennylane.math` providing tensor-library agnostic functions."""
 
+import autoray as ar
 from pennylane import math as pl_math
 
 from .matrix_manipulation import expand_matrix, expand_vector
+
+dag = ar.dag
 
 
 def __getattr__(name):
@@ -14,8 +17,8 @@ def __getattr__(name):
 
 def __dir__():
     # Include the functions from pennylane math library in the dir() output
-    our_stuff = set(list(globals().keys())) - {"pl_math"}
+    our_stuff = set(list(globals().keys())) - {"pl_math", "ar"}
     return list(our_stuff | set(pl_math.__dir__()))
 
 
-__all__ = ["expand_matrix", "expand_vector"] + pl_math.__all__
+__all__ = ["expand_matrix", "expand_vector", "dag"] + pl_math.__all__
